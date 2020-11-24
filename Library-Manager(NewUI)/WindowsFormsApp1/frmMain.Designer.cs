afa78b7ef974d83ef790061e34e5951b2f64a65a
UIT-19521484 <19521484@gm.uit.edu.vn>
2020-11-24 19:36:52 +0700

    recommit

--- Library-Manager(NewUI)/WindowsFormsApp1/frmMain.Designer.cs
+++ Library-Manager(NewUI)/WindowsFormsApp1/frmMain.Designer.cs
@@ -53,17 +53,18 @@ namespace WindowsFormsApp1
             this.btnLogout = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             this.btnMenu = new Guna.UI2.WinForms.Guna2Button();
-            this.ctrboxMinimize = new Guna.UI2.WinForms.Guna2ControlBox();
             this.pnlChildForm = new Guna.UI2.WinForms.Guna2Panel();
             this.rsboxBottom = new Guna.UI2.WinForms.Guna2ResizeBox();
             this.rsboxRight = new Guna.UI2.WinForms.Guna2ResizeBox();
-            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.ctrboxMaximize = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.rsboxBR = new Guna.UI2.WinForms.Guna2ResizeBox();
             this.ctrboxExit = new Guna.UI2.WinForms.Guna2ControlBox();
-            this.guna2ResizeBox1 = new Guna.UI2.WinForms.Guna2ResizeBox();
+            this.ctrboxMaximize = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.ctrboxMinimize = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.pnlTopBar = new Guna.UI2.WinForms.Guna2Panel();
+            this.DragControl_pnlTopBar = new Guna.UI2.WinForms.Guna2DragControl(this.components);
             this.pnlMenu.SuspendLayout();
             this.guna2Panel2.SuspendLayout();
-            this.guna2Panel1.SuspendLayout();
+            this.pnlTopBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // BorderlessForm
@@ -118,6 +119,7 @@ namespace WindowsFormsApp1
             this.btnEmployee.Image = global::WindowsFormsApp1.Properties.Resources.icons8_employee_64px;
             this.btnEmployee.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnEmployee.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnEmployee.IndicateFocus = true;
             this.btnEmployee.Location = new System.Drawing.Point(0, 339);
             this.btnEmployee.Name = "btnEmployee";
             this.btnEmployee.PressedColor = System.Drawing.Color.DeepSkyBlue;
@@ -136,13 +138,14 @@ namespace WindowsFormsApp1
             this.btnReader.CustomImages.Parent = this.btnReader;
             this.btnReader.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnReader.FillColor = System.Drawing.Color.Transparent;
-            this.btnReader.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReader.Font = new System.Drawing.Font("Arial", 14.25F);
             this.btnReader.ForeColor = System.Drawing.Color.White;
             this.btnReader.HoverState.FillColor = System.Drawing.Color.LightSkyBlue;
             this.btnReader.HoverState.Parent = this.btnReader;
             this.btnReader.Image = global::WindowsFormsApp1.Properties.Resources.icons8_reading_64px;
             this.btnReader.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnReader.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnReader.IndicateFocus = true;
             this.btnReader.Location = new System.Drawing.Point(0, 270);
             this.btnReader.Name = "btnReader";
             this.btnReader.PressedColor = System.Drawing.Color.DeepSkyBlue;
@@ -161,13 +164,14 @@ namespace WindowsFormsApp1
             this.btnBooks.CustomImages.Parent = this.btnBooks;
             this.btnBooks.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnBooks.FillColor = System.Drawing.Color.Transparent;
-            this.btnBooks.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBooks.Font = new System.Drawing.Font("Arial", 14.25F);
             this.btnBooks.ForeColor = System.Drawing.Color.White;
             this.btnBooks.HoverState.FillColor = System.Drawing.Color.LightSkyBlue;
             this.btnBooks.HoverState.Parent = this.btnBooks;
             this.btnBooks.Image = global::WindowsFormsApp1.Properties.Resources.icons8_book_stack_64px;
             this.btnBooks.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnBooks.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnBooks.IndicateFocus = true;
             this.btnBooks.Location = new System.Drawing.Point(0, 201);
             this.btnBooks.Name = "btnBooks";
             this.btnBooks.PressedColor = System.Drawing.Color.DeepSkyBlue;
@@ -186,13 +190,14 @@ namespace WindowsFormsApp1
             this.btnInfo.CustomImages.Parent = this.btnInfo;
             this.btnInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnInfo.FillColor = System.Drawing.Color.Transparent;
-            this.btnInfo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfo.Font = new System.Drawing.Font("Arial", 14.25F);
             this.btnInfo.ForeColor = System.Drawing.Color.White;
             this.btnInfo.HoverState.FillColor = System.Drawing.Color.LightSkyBlue;
             this.btnInfo.HoverState.Parent = this.btnInfo;
             this.btnInfo.Image = global::WindowsFormsApp1.Properties.Resources.icons8_top_menu_64px;
             this.btnInfo.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnInfo.ImageSize = new System.Drawing.Size(40, 40);
+            this.btnInfo.IndicateFocus = true;
             this.btnInfo.Location = new System.Drawing.Point(0, 132);
             this.btnInfo.Name = "btnInfo";
             this.btnInfo.PressedColor = System.Drawing.Color.DeepSkyBlue;
@@ -276,28 +281,13 @@ namespace WindowsFormsApp1
             this.btnMenu.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
             // 
-            // ctrboxMinimize
-            // 
-            this.ctrboxMinimize.BackColor = System.Drawing.Color.Transparent;
-            this.ctrboxMinimize.BorderColor = System.Drawing.Color.Transparent;
-            this.ctrboxMinimize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
-            this.ctrboxMinimize.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ctrboxMinimize.FillColor = System.Drawing.Color.Transparent;
-            this.ctrboxMinimize.HoverState.Parent = this.ctrboxMinimize;
-            this.ctrboxMinimize.IconColor = System.Drawing.Color.Black;
-            this.ctrboxMinimize.Location = new System.Drawing.Point(845, 0);
-            this.ctrboxMinimize.Name = "ctrboxMinimize";
-            this.ctrboxMinimize.ShadowDecoration.Parent = this.ctrboxMinimize;
-            this.ctrboxMinimize.Size = new System.Drawing.Size(45, 30);
-            this.ctrboxMinimize.TabIndex = 1;
-            // 
             // pnlChildForm
             // 
             this.pnlChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlChildForm.Location = new System.Drawing.Point(220, 30);
             this.pnlChildForm.Name = "pnlChildForm";
             this.pnlChildForm.ShadowDecoration.Parent = this.pnlChildForm;
-            this.pnlChildForm.Size = new System.Drawing.Size(980, 660);
+            this.pnlChildForm.Size = new System.Drawing.Size(980, 665);
             this.pnlChildForm.TabIndex = 4;
             // 
             // rsboxBottom
@@ -306,9 +296,9 @@ namespace WindowsFormsApp1
             this.rsboxBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.rsboxBottom.FillColor = System.Drawing.Color.Transparent;
             this.rsboxBottom.ForeColor = System.Drawing.Color.Empty;
-            this.rsboxBottom.Location = new System.Drawing.Point(220, 690);
+            this.rsboxBottom.Location = new System.Drawing.Point(220, 695);
             this.rsboxBottom.Name = "rsboxBottom";
-            this.rsboxBottom.Size = new System.Drawing.Size(980, 10);
+            this.rsboxBottom.Size = new System.Drawing.Size(980, 5);
             this.rsboxBottom.TabIndex = 0;
             this.rsboxBottom.TargetControl = this;
             // 
@@ -320,36 +310,21 @@ namespace WindowsFormsApp1
             this.rsboxRight.ForeColor = System.Drawing.Color.Empty;
             this.rsboxRight.Location = new System.Drawing.Point(1195, 30);
             this.rsboxRight.Name = "rsboxRight";
-            this.rsboxRight.Size = new System.Drawing.Size(5, 660);
+            this.rsboxRight.Size = new System.Drawing.Size(5, 665);
             this.rsboxRight.TabIndex = 1;
             this.rsboxRight.TargetControl = this;
             // 
-            // guna2Panel1
+            // rsboxBR
             // 
-            this.guna2Panel1.Controls.Add(this.ctrboxMinimize);
-            this.guna2Panel1.Controls.Add(this.ctrboxMaximize);
-            this.guna2Panel1.Controls.Add(this.ctrboxExit);
-            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.guna2Panel1.Location = new System.Drawing.Point(220, 0);
-            this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(980, 30);
-            this.guna2Panel1.TabIndex = 6;
-            // 
-            // ctrboxMaximize
-            // 
-            this.ctrboxMaximize.BackColor = System.Drawing.Color.Transparent;
-            this.ctrboxMaximize.BorderColor = System.Drawing.Color.Transparent;
-            this.ctrboxMaximize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
-            this.ctrboxMaximize.Dock = System.Windows.Forms.DockStyle.Right;
-            this.ctrboxMaximize.FillColor = System.Drawing.Color.Transparent;
-            this.ctrboxMaximize.HoverState.Parent = this.ctrboxMaximize;
-            this.ctrboxMaximize.IconColor = System.Drawing.Color.Black;
-            this.ctrboxMaximize.Location = new System.Drawing.Point(890, 0);
-            this.ctrboxMaximize.Name = "ctrboxMaximize";
-            this.ctrboxMaximize.ShadowDecoration.Parent = this.ctrboxMaximize;
-            this.ctrboxMaximize.Size = new System.Drawing.Size(45, 30);
-            this.ctrboxMaximize.TabIndex = 8;
+            this.rsboxBR.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.rsboxBR.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.rsboxBR.FillColor = System.Drawing.Color.Transparent;
+            this.rsboxBR.ForeColor = System.Drawing.Color.Empty;
+            this.rsboxBR.Location = new System.Drawing.Point(1190, 689);
+            this.rsboxBR.Name = "rsboxBR";
+            this.rsboxBR.Size = new System.Drawing.Size(10, 10);
+            this.rsboxBR.TabIndex = 0;
+            this.rsboxBR.TargetControl = this;
             // 
             // ctrboxExit
             // 
@@ -366,17 +341,52 @@ namespace WindowsFormsApp1
             this.ctrboxExit.Size = new System.Drawing.Size(45, 30);
             this.ctrboxExit.TabIndex = 9;
             // 
-            // guna2ResizeBox1
+            // ctrboxMaximize
             // 
-            this.guna2ResizeBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.guna2ResizeBox1.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
-            this.guna2ResizeBox1.FillColor = System.Drawing.Color.Transparent;
-            this.guna2ResizeBox1.ForeColor = System.Drawing.Color.Empty;
-            this.guna2ResizeBox1.Location = new System.Drawing.Point(1190, 689);
-            this.guna2ResizeBox1.Name = "guna2ResizeBox1";
-            this.guna2ResizeBox1.Size = new System.Drawing.Size(10, 10);
-            this.guna2ResizeBox1.TabIndex = 0;
-            this.guna2ResizeBox1.TargetControl = this;
+            this.ctrboxMaximize.BackColor = System.Drawing.Color.Transparent;
+            this.ctrboxMaximize.BorderColor = System.Drawing.Color.Transparent;
+            this.ctrboxMaximize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox;
+            this.ctrboxMaximize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ctrboxMaximize.FillColor = System.Drawing.Color.Transparent;
+            this.ctrboxMaximize.HoverState.Parent = this.ctrboxMaximize;
+            this.ctrboxMaximize.IconColor = System.Drawing.Color.Black;
+            this.ctrboxMaximize.Location = new System.Drawing.Point(890, 0);
+            this.ctrboxMaximize.Name = "ctrboxMaximize";
+            this.ctrboxMaximize.ShadowDecoration.Parent = this.ctrboxMaximize;
+            this.ctrboxMaximize.Size = new System.Drawing.Size(45, 30);
+            this.ctrboxMaximize.TabIndex = 8;
+            // 
+            // ctrboxMinimize
+            // 
+            this.ctrboxMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.ctrboxMinimize.BorderColor = System.Drawing.Color.Transparent;
+            this.ctrboxMinimize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.ctrboxMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ctrboxMinimize.FillColor = System.Drawing.Color.Transparent;
+            this.ctrboxMinimize.HoverState.Parent = this.ctrboxMinimize;
+            this.ctrboxMinimize.IconColor = System.Drawing.Color.Black;
+            this.ctrboxMinimize.Location = new System.Drawing.Point(845, 0);
+            this.ctrboxMinimize.Name = "ctrboxMinimize";
+            this.ctrboxMinimize.ShadowDecoration.Parent = this.ctrboxMinimize;
+            this.ctrboxMinimize.Size = new System.Drawing.Size(45, 30);
+            this.ctrboxMinimize.TabIndex = 1;
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.Controls.Add(this.ctrboxMinimize);
+            this.pnlTopBar.Controls.Add(this.ctrboxMaximize);
+            this.pnlTopBar.Controls.Add(this.ctrboxExit);
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(220, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.ShadowDecoration.Parent = this.pnlTopBar;
+            this.pnlTopBar.Size = new System.Drawing.Size(980, 30);
+            this.pnlTopBar.TabIndex = 6;
+            // 
+            // DragControl_pnlTopBar
+            // 
+            this.DragControl_pnlTopBar.ContainerControl = this;
+            this.DragControl_pnlTopBar.TargetControl = this.pnlTopBar;
             // 
             // frmMain
             // 
@@ -384,11 +394,11 @@ namespace WindowsFormsApp1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(1200, 700);
-            this.Controls.Add(this.guna2ResizeBox1);
+            this.Controls.Add(this.rsboxBR);
             this.Controls.Add(this.rsboxRight);
             this.Controls.Add(this.pnlChildForm);
             this.Controls.Add(this.rsboxBottom);
-            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.pnlTopBar);
             this.Controls.Add(this.pnlMenu);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -396,7 +406,7 @@ namespace WindowsFormsApp1
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.pnlMenu.ResumeLayout(false);
             this.guna2Panel2.ResumeLayout(false);
-            this.guna2Panel1.ResumeLayout(false);
+            this.pnlTopBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -406,7 +416,6 @@ namespace WindowsFormsApp1
         private Guna.UI2.WinForms.Guna2BorderlessForm BorderlessForm;
         private System.Windows.Forms.Timer tmrSideMenuAnimation;
         private Guna.UI2.WinForms.Guna2DragControl DragControl_pnlMenu;
-        private Guna.UI2.WinForms.Guna2ControlBox ctrboxMinimize;
         private Guna.UI2.WinForms.Guna2Panel pnlChildForm;
         private Guna.UI2.WinForms.Guna2Panel pnlMenu;
         private Guna.UI2.WinForms.Guna2Button btnEmployee;
@@ -419,9 +428,11 @@ namespace WindowsFormsApp1
         private Guna.UI2.WinForms.Guna2Button btnMenu;
         private Guna.UI2.WinForms.Guna2ResizeBox rsboxRight;
         private Guna.UI2.WinForms.Guna2ResizeBox rsboxBottom;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2ResizeBox rsboxBR;
+        private Guna.UI2.WinForms.Guna2Panel pnlTopBar;
+        private Guna.UI2.WinForms.Guna2ControlBox ctrboxMinimize;
         private Guna.UI2.WinForms.Guna2ControlBox ctrboxMaximize;
         private Guna.UI2.WinForms.Guna2ControlBox ctrboxExit;
-        private Guna.UI2.WinForms.Guna2ResizeBox guna2ResizeBox1;
+        private Guna.UI2.WinForms.Guna2DragControl DragControl_pnlTopBar;
     }
 }
--- Library_Manager_UI/Program.cs
+++ Library_Manager_UI/Program.cs
@@ -16,7 +16,7 @@ namespace Library_Manager_UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmMain());
         }
     }
 }
